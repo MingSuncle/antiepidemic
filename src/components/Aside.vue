b927abced4ded9f1e1211b502f2372916ec6f5bb
zza <3161582599@qq.com>
2022-09-12 23:34:19 +0800

    前端代码

---
+++ src/components/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-menu :default-openeds="['2']" style="min-height:100%;overflow-x: hidden;" background-color="rgb(48,65,86)"
+        text-color='#fff' active-text-color="#2990d6" :collapse-transition="true" :collapse="isCollapse" router>
+        <div style="height:60px;line-height: 60px;text-align: center;">
+            <img src="../assets/logoa.png" alt="" style="width:20px;position:relative;top:5px;margin-right: 5px;">
+            <b style="color:white " v-show="logoTextShow">企业防疫官</b>
+        </div>
+        <el-menu-item index="/">
+            <template slot="title">
+            <i class="el-icon-house"></i>
+            <span slot="title">主页</span>
+        </template>
+        </el-menu-item>
+        <el-submenu index="2">
+            <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span slot="title">系统管理</span>
+            </template>
+            <el-menu-item index="/user">用户管理</el-menu-item>
+        </el-submenu>
+
+    </el-menu>
+</template>
+
+<script>
+    export default{
+        name:"Aside",
+        props:{
+            isCollapse:Boolean,
+            logoTextShow:Boolean
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Manage',
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    meta:{
+      title:'首页'
+    },
+    children: [{
+      path: 'user',
+      name: 'User',
+      meta:{
+        title:'员工管理'
+      },
+      component: () => import('../views/User.vue'),
+    },
+    {
+      path: 'home',
+      name: 'Home',
+      component: () => import('../views/Home.vue'),
+    },
+    ]
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
